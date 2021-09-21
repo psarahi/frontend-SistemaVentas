b1e38f9ba374ec00667b8e7e45b89ec3a323200e
Lesly Peña <pleslysarahi@gmail.com>
2021-09-21 15:15:34 -0600

    cambio de backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3003/api'
-  apiUrl: 'https://appsistema-ventas.herokuapp.com'
+  apiUrl: 'https://appsistema-ventas.herokuapp.com/api'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://appsistema-ventas.herokuapp.com'
+  apiUrl: 'https://appsistema-ventas.herokuapp.com/api'
 };
 
 /*
